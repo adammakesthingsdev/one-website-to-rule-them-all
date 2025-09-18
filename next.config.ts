f06f57786fcc0f6d7da3379726615914690ae0b3
adammakesthingsdev <adam@adammakesthings.dev>
2025-09-18 16:38:51 -0400

    Revert "feat: add portfolio page and database"
    This reverts commit 77c5415d7dca9007b465c17087f3087a137b254d.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    output: "standalone",
+  /* config options here */
+  output: "standalone",
 };
 
-const withMDX = createMDX({
-    // Add markdown plugins here, as desired
-});
-
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
--- src/mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-
-const components: MDXComponents = {
-    ul: ({ children }) => <ul className="list-disc list-inside">{children}</ul>,
-} satisfies MDXComponents;
-
-export function useMDXComponents(): MDXComponents {
-    return components;
-}
